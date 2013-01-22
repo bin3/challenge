926b45aa8c0483f8ad8f154485fcd6a33af68296
Binson Zhang <bin183cs@gmail.com>
2013-01-22 23:06:35 +0800

    Add SameTree.cpp

---
+++ src/leetcode/SameTree.cpp
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2013 Binson Zhang.
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @author	Binson Zhang <bin183cs@gmail.com>
+ * @date		2013-1-22
+ */
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+#include "util_types.h"
+
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode *p, TreeNode *q) {
+        if (!p && !q) return true;
+        if (!p || !q) return false;
+        if (p->val != q->val) return false;
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+    }
+};
+
+#include "util.h"
+
+int main(int argc, char **argv) {
+  std::cout << "------" << argv[0] << "------" << std::endl;
+
+  Solution sol;
+
+  return 0;
+}
