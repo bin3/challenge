a79f8fe8bc79e99236eac1244468b9d89eeeadba
Binson Zhang <bin183cs@gmail.com>
2013-03-11 07:55:27 +0800

    Add RotateList.cpp

---
+++ src/leetcode/RotateList.cpp
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2013 Binson Zhang.
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @author	Binson Zhang <bin183cs@gmail.com>
+ * @date		2013-3-11
+ */
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *rotateRight(ListNode *head, int k) {
+        if (!head) return head;
+        ListNode* p = head;
+        ListNode* tail = NULL;
+        int n = 0;
+        while (p) {
+            ++n;
+            tail = p;
+            p = p->next;
+        }
+        int move = n - k % n;
+        if (move == n) return head;
+        p = head;
+        for (int i = 1; i < move; ++i) p = p->next;
+        tail->next = head;
+        head = p->next;
+        p->next = NULL;
+        return head;
+    }
+};
+
+#include "util.h"
+
+int main(int argc, char **argv) {
+  std::cout << "------" << argv[0] << "------" << std::endl;
+
+  Solution sol;
+
+  return 0;
+}
