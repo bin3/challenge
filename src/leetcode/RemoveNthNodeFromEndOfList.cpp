933b3c2f84792f39538faa2fa622965952de06b7
Binson Zhang <bin183cs@gmail.com>
2013-03-12 16:30:56 +0800

    Add RemoveNthNodeFromEndOfList.cpp

---
+++ src/leetcode/RemoveNthNodeFromEndOfList.cpp
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2013 Binson Zhang.
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @author	Binson Zhang <bin183cs@gmail.com>
+ * @date		2013-3-12
+ */
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <algorithm>
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *removeNthFromEnd(ListNode *head, int n) {
+        if (!head || n <= 0) return head;
+        ListNode* f = head;
+        ListNode* cur = head;
+        ListNode* prv = NULL;
+        while (--n > 0) {
+            f = f->next;
+        }
+        while (f->next) {
+            f = f->next;
+            prv = cur;
+            cur = cur->next;
+        }
+        if (prv) prv->next = cur->next;
+        else head = head->next;
+        return head;
+    }
+};
+
+#include "../util.h"
+
+int main(int argc, char **argv) {
+  std::cout << "------" << argv[0] << "------" << std::endl;
+
+  Solution sol;
+
+  return 0;
+}
